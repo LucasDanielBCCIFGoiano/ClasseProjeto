ddf108dff67de0c3738df8889bf78dc3a7c3a557
Lucas Daniel <lucasfelepe@gmail.com>
2021-08-15 22:37:36 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasseProjeto
+{
+    static class Calculadora
+    {
+        public static decimal Soma (decimal x, decimal y){
+            return x + y;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ClasseProjeto;
+using System;
+
+namespace ClasseHerancaProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculadora.Soma(25, 50));
+        }
+    }
+}
